bd80fee03d877fd807ca06b36944f7693d4350ab
Anton Österberg <anton.osterberg@gmail.com>
2019-01-28 00:00:22 +0100

    Do not throw on missing value

--- src/main/kotlin/com/mantono/config/ContextConfig.kt
+++ src/main/kotlin/com/mantono/config/ContextConfig.kt
@@ -8,7 +8,7 @@ data class ContextConfig(private val config: com.typesafe.config.Config): Config
         ?: throw RuntimeException("No value found for property '$key' and no default value given")
     override operator fun get(key: String, default: Any): String = getOrNull(key) ?: default.toString()
 
-    override fun getOrNull(key: String): String? = config.getString(key)
+    override fun getOrNull(key: String): String? = if(config.hasPath(key)) config.getString(key) else null
 
     fun withContext(context: String): ContextConfig = ContextConfig(config.getConfig(context))
 }
